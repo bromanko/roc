567aaf752f263be82d34d1bae2fd66eedab921c2
Brian Romanko <hello@bromanko.com>
2024-06-05 20:05:23 -0700

    Add flake-parts

--- flake.nix
+++ flake.nix
@@ -1,15 +1,22 @@
 {
-  description = "A very basic flake";
+  description = "Experimenting with roc";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "aarch64-darwin" ];
+      perSystem = { pkgs, system, inputs', ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [ ];
+        };
+        packages = { };
+        devShells = { default = pkgs.mkShell { buildInputs = [ ]; }; };
+      };
+    };
 }
